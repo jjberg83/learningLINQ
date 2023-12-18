c5888ec8a73b1ab3c17d9d781dd45d8241c65e15
Jørund Jenssen Berg <jjberg@gmail.com>
2023-12-18 13:58:47 +0100

    First dive into LINQ functionality, simulating shuffling of cards

---
+++ Extensions.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqFaroShuffle
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while(firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+
+        public static bool CheckSequenceEquality<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while((firstIter.MoveNext() == true) && secondIter.MoveNext())
+            {
+                if((firstIter.Current != null) && !firstIter.Current.Equals(secondIter.Current))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using LinqFaroShuffle;
+
+// query syntax
+// var startingDeck = from s in Suits()
+//                    from r in Ranks()
+//                    select new { Suit = s, Rank = r};
+
+// method syntax
+var startingDeck = Suits().SelectMany(s => Ranks().Select(r => new { Suit = s, Rank = r}));
+var shuffle = startingDeck;
+
+int times = 0;
+
+
+shuffle = startingDeck;
+do
+{
+    shuffle = shuffle.Skip(26).InterleaveSequenceWith(shuffle.Take(26));
+    times++;
+} while(!shuffle.CheckSequenceEquality(startingDeck));
+      
+Console.WriteLine(times);
+
+
+
+
+
+
+static IEnumerable<string> Suits()
+{
+    yield return "hearts";
+    yield return "diamonds";
+    yield return "clubs";
+    yield return "spades";
+}
+
+static IEnumerable<string> Ranks() 
+{
+    yield return "two";
+    yield return "three";
+    yield return "four";
+    yield return "five";
+    yield return "six";
+    yield return "seven";
+    yield return "eight";
+    yield return "nine";
+    yield return "ten";
+    yield return "jack";
+    yield return "queen";
+    yield return "king";
+    yield return "ace";
+}
